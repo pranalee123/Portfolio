418bfe1257c6410eee2de01454107255e578ff5a
Pranalee Dabhade <pranali123@gmail.com>
2021-05-28 19:22:48 -0500

    fixed sliding menu when links are open

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const sectionHeader = document.querySelector('.section1_header');
 const closeMenu = document.querySelector('.closemenu');
 const openMenu = document.querySelector('.openmenu');
+const headerLinks = document.querySelectorAll('.section1_header__link');
 
+const show = () => {
+    sectionHeader.style.display = 'flex';
+    sectionHeader.style.top = '0';
+}
 
+const close = () => {
+    sectionHeader.style.top = '-100%';
+}
 
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
-
-function show() {
-    sectionHeader.style.display = 'flex';
-    sectionHeader.style.top = '0';
-
-}
-
-function close() {
-    sectionHeader.style.top = '-100%';
-}
+headerLinks.forEach((l) => { l.addEventListener('click', close) })
 
 show();
 close();
